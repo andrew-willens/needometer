70beb36daf3b3f30a0ebeacc7790f69bacc469a8
Andrew Willens <andrew.willens@gmail.com>
2014-03-24 14:21:54 -0400

    projects.csv included

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,6 +1,7 @@
 var initD3 = function(width) {
 	var	width = width,
-			height = width / 2;
+			height = width / 2
+			active = d3.select(null);
 
 	var projection = d3.geo.albersUsa()
 		.translate([width / 2, height / 2]);
@@ -13,26 +14,51 @@ var initD3 = function(width) {
 			.attr("width", width)
 			.attr("height", height)
 
-	d3.json("./data/us_with_projects.json", function(err, us) {
-		var counties = topojson.feature(us, us.objects.counties2).features;
-
-		svg.selectAll("path")
-				.data(counties)
-			.enter().append("path")
-			.attr("class", "county")
-			.style("fill", function(d){
-				if (d.properties.projects.length>0){
-					return "red";
-				}
-			})
-			.attr("d", path)
-			.on("click", function(d){
-				console.log(d);
-			});
+	d3.json("./data/us.json", function(err, us) {
+		// var counties = topojson.feature(us, us.objects.uscounties).features;
+		var states = topojson.feature(us, us.objects.states).features;
+		var land = topojson.feature(us, us.objects.usland);
 
 		// svg.append("path")
-				// .datum(topojson.mesh(us, us.objects.uscounties, function(a,b) { return a !== b; }))
-				// .attr("id", "state-borders")
-				// .attr("d", path);
+  //     .datum(land)
+  //     .attr("class", "land")
+  //   	.attr("d", path);
+
+    svg.selectAll("path")
+				.data(states)
+			.enter().append("path")
+			.attr("id", function(d) {
+				return d.properties.STUSPS;
+			})
+			.attr("class", "state")
+			.attr("d", path)
+			.on("click", function(){
+		  	clicked();
+		  	svg.append("path")
+					.datum(topojson.mesh(us, us.objects.uscounties, function(a, b) { return a !== b && !(a.id / 1000 ^ b.id / 1000) }))
+					.attr("class", "county-border")
+					.attr("d", path);
+      });
+
+		// svg.selectAll("path")
+		// 		.data(counties)
+		// 	.enter().append("path")
+		// 	.attr("class", "county")
+		// 	// .style("fill", function(d){
+		// 	// 	if (d.properties.projects.length>0){
+		// 	// 		return "red";
+		// 	// 	}
+		// 	// })
+		// 	.attr("d", path)
+		// 	.on("click", function(d){
+		// 		console.log(d);
+		// 	});
+
+		svg.append("path")
+			.datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
+			.attr("class", "state-border")
+			.attr("d", path);
+
 	});
+
 };
