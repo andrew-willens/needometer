b528ed846a17bba66a13d448cea6eb2d61183740
Andrew Willens <andrew.willens@gmail.com>
2014-03-28 19:08:00 -0400

    looking back at master

--- public/js/services/ChartService.js
+++
@@ -1,3 +0,0 @@
-angular.module('ChartService', []).factory('Chart', ['$http', function($http){
-
-}]);
--- public/js/services/MainService.js
+++
@@ -1,6 +0,0 @@
-angular.module('MainService', []).factory('Main', ['$http', function($http){
-		//GET
-		// get : function() {
-		// 	return $http.get('/');
-		// }
-}]);
