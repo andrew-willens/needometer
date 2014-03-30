9af3d5b1b12898821642f6e5ea0e5fa95e55fb91
Andrew Willens <andrew.willens@gmail.com>
2014-03-29 21:17:58 -0400

    dataChef now formats for direct injection into d3

--- public/js/charts.js
+++ public/js/charts.js
@@ -1,4 +1,5 @@
-var pieChart = function(width) {
+var pieChart = function(data) {
+	console.log(data)
   var width = 960/2.5,
       height = 500/2.5,
       radius = Math.min(width, height) / 2;
@@ -8,25 +9,18 @@ var pieChart = function(width) {
       .range(["red", "orange", "yellow", "green", "blue", "purple", "gray"]);
 
   var arc = d3.svg.arc()
-      .outerRadius(radius - 10)
-      .innerRadius(0);
+      .outerRadius(radius - 0)
+      .innerRadius(50);
 
   var pie = d3.layout.pie()
       .sort(null)
-      .value(function(d) { return d.population; });
-
+      .value(function(d) { return d.count; });
 
   var svg = d3.select("#pieChartDiv").append("svg")
       .attr("width", width)
       .attr("height", height)
     .append("g")
       .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-  d3.csv("data/pie_chart.csv", function(error, data) {
-
-    data.forEach(function(d) {
-      d.population = +d.population;
-    });
 
     var g = svg.selectAll(".arc")
         .data(pie(data))
@@ -35,14 +29,13 @@ var pieChart = function(width) {
 
     g.append("path")
         .attr("d", arc)
-        .style("fill", function(d) { return color(d.data.age); });
+        .style("fill", function(d) { return color(d.data.count); });
 
     g.append("text")
         .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
         .attr("dy", ".35em")
         .style("text-anchor", "middle")
-        .text(function(d) { return d.data.age; });
-});
+        .text(function(d) { return d.data.count+" "+d.data.type; });
 };
 
 ///////////////
